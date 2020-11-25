3608f06eed62c5ab7cf89d9475977bf83d9d1655
Papa-het <yerkinmm@gmail.com>
2020-11-25 21:16:12 +0600

    perf: bubleSort

--- sorting/bubleSort.cpp
+++ sorting/bubleSort.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 
+std::string bubleSort(std::string arr);
+
 int main()
 {
     using namespace std;
 
-    char arr[] = "47586321";
+    string arr;
 
-    for (int i = 0, length = strlen(arr); i < length; i++)
+    cout << "Gimme sequence of integers: ";
+    cin >> arr;
+    cout << "You gave me this sequence of integers: " << arr << endl;
+
+    cout << bubleSort(arr) << endl;
+    return 0;
+}
+
+std::string bubleSort(std::string arr)
+{
+    for (int i = 0, length = arr.length(); i < length; i++)
     {
         for (int j = 1; j < length; j++)
         {
             if (arr[j] < arr[j - 1])
             {
-                int tmp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = tmp;
+                int tmp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
             }
         }
     }
 
-    cout << "Result: " << arr << endl;
-    return 0;
+    return arr;
 }
