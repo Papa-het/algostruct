3d711976d381e73bddab7c46fb1f8482692054c3
Papa-het <yerkinmm@gmail.com>
2021-01-13 20:28:06 +0600

    leetcode

--- basics/1_bot.py
+++
@@ -1,6 +0,0 @@
-print("- Hello! What is your name?")
-name = input("> ")
-print("- I'm glad to see you", name, "!")
-print("- What is your age?")
-age = int(input("> "))
-print("- I thought you are", age + 1, "years old. You look younger!")
