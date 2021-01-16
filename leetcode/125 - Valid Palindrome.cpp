bcc7f7b9a8e14eec1d66d0fa91ed600bd2dbd506
Papa-het <yerkinmm@gmail.com>
2021-01-17 01:27:24 +0600

    feat(leetcode): add 125 problem

---
+++ leetcode/125 - Valid Palindrome.cpp
@@ -0,0 +1,40 @@
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+// Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+// Example 1:
+
+// Input: "A man, a plan, a canal: Panama"
+// Output: true
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(isalnum(s[i])) {
+                str += s[i];
+            }
+        }
+        
+        for(auto & c: str) {
+            c = tolower(c);
+        }
+        
+        int i = 0;
+        int j = str.length() - 1;
+        
+        while(i < j) {
+            if(str[i] != str[j]) {
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
