a2edea4c51fa3f4912eff8750f777676bea8c97d
Papa-het <yerkinmm@gmail.com>
2021-01-16 21:21:16 +0600

    feat(leetcode): add 804 problem

---
+++ leetcode/804 - Unique Morse Code Words.cpp
@@ -0,0 +1,45 @@
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+
+// Return the number of different transformations among all words we have.
+
+
+// Example:
+// Input: words = ["gin", "zen", "gig", "msg"]
+// Output: 2
+// Explanation: 
+// The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+
+// There are 2 different transformations, "--...-." and "--...--.".
+
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        int counter = 0;
+        vector<string> temp;
+        vector<string> morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        for(int i=0; i < words.size(); i++) {
+        string word;
+        for(int j=0; j < words[i].size(); j++) {
+          word.append(morse[((int)words[i][j] - 97)]);
+        }
+
+        if(find(temp.begin(), temp.end(), word) != temp.end()) {
+          continue;
+        } else {
+          temp.push_back(word);
+          counter++; 
+        }
+        }
+        return counter;
+    }
+};
